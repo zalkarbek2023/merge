20ca25552a865c9fa3eada087e5528bfa027cc64
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2023-06-29 13:43:20 +0600

    Initial Commit

---
+++ Merge.git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Merge.git
+//
+//  Created by zalkarbek on 29/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
